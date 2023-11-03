ee17431f2f738c5f0f75f6105688c72e962fed09
danelian <141317269+danelian@users.noreply.github.com>
2023-11-03 04:59:58 +0300

    project setup
    vue & vite
    install chart.js

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref, shallowRef, computed, watch, nextTick } from 'vue'
+import Chart from 'chart.js/auto'
+</script>
+
+<template>
+  <h1>Weight Tracker</h1>
+</template>
+
+<style scoped>
+
+</style>
